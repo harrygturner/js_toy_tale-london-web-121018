e15cf2d92d6f7654c237ab0684dbba0af098472d
github username <github email address>
2019-02-08 08:33:30 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
-let addToyForm = false
-
-// YOUR CODE HERE
+let addToy = false
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
-  addToyForm = !addToyForm
-  if (addToyForm) {
+  addToy = !addToy
+  if (addToy) {
     toyForm.style.display = 'block'
     // submit listener here
   } else {
@@ -15,100 +13,83 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-// event listener on add new toy button
-const submitBtn = document.querySelector('.submit')
-submitBtn.addEventListener('click', event => {
-  event.preventDefault();
-  const toyName = document.querySelector('.input-text').value
-  const toyImage = document.querySelectorAll('.input-text')[1].value
-  addToyToServer(toyName, toyImage)
-    .then( toy => addToy(toy.id, toy.name, toy.image, toy.likes ));
-  toyForm.reset();
-})
+// --------------------------------------------------------------
 
+const toysContainer = document.querySelector('#toy-collection');
+const toyFormEl = document.querySelector('.add-toy-form');
 
-// OR HERE!
-
-const toysContainer = document.querySelector('#toy-collection')
-
-// -----------------------------------------
-
-// add a single toy to the page
-
-const addToy = (id, name, image, likes) => {
+// render a single toy to page
+function renderToy(toy) {
   const toyEl = document.createElement('div')
-  toyEl.className = `card toy-${id}`
+  toyEl.className = `card toy-${toy.id}`
   toyEl.innerHTML = `
-  <h2>${name}</h2>
-  <img class='toy-avatar' src=${image}>
-  <p>${likes} Likes</p>
-  <button class="like-btn" data-id=${id}>Like <3</button>
+    <h2>${toy.name}</h2>
+    <img class='toy-avatar' src=${toy.image}>
+    <p>${toy.likes} Likes</p>
+    <button class="like-btn" data-id=${toy.id}>Like <3</button>
   `
-  toysContainer.appendChild(toyEl)
+  const likeBtn = toyEl.querySelector('.like-btn');
+  likeBtn.addEventListener('click', () => {
+    toy.likes++
+    toyEl.querySelector('p').innerText = `${toy.likes} Likes`;
+    updateToy(toy);
+  })
+  toysContainer.appendChild(toyEl);
 }
 
-// add multiple toys on the page
-
-const addToys = toys => {
-  for (const toy of toys){
-    addToy(toy.id, toy.name, toy.image, toy.likes)
-  }
+// render all toys onto page
+function renderToys(toys) {
+  toysContainer.innerHTML = "";
+  toys.forEach(renderToy);
 }
 
-// update toy likes
-const updateToyCard = toy => {
-  const toyLikeEl = document.querySelector(`#toy-collection .toy-${toy.id} p`)
-  toyLikeEl.innerText = `${toy.likes} Likes`
+// event listener for creating a new toy
+function addEventListenerToForm() {
+  toyFormEl.addEventListener('submit', () => {
+    const newToy = {
+      "name": toyFormEl.name.value,
+      "image": toyFormEl.image.value,
+      "likes": 0
+    }
+    createToy(newToy)
+      .then(renderToy);
+  })
 }
 
-// event listener on like button
-document.addEventListener('click', event => {
-  if(event.target.className === 'like-btn'){
-    let toyId = event.target.dataset.id
-    getToy(parseInt(toyId))
-      .then( toy => updateToyLikes(toy.id, toy.name, toy.image, toy.likes))
-      .then(updateToyCard);
-  }
-})
+// --------------------- Server Stuff -------------------------
 
-
-// remove all toy cards
-const removeToys = () => {
-    toysContainer.innerHTML = ""
-}
-
-// Server Stuff
-
-// Will return an array of toy objects
-const getToys = () => {
+// get toys from server
+function getToys() {
   return fetch('http://localhost:3000/toys')
     .then(resp => resp.json())
 }
 
-
-getToys().then(addToys)
-
-// Add created toy to server and returns the created toy object
-const addToyToServer = (name, image) => {
+// add toy to server
+function createToy(toy) {
   return fetch('http://localhost:3000/toys', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name: name, image: image, likes: 0})
+    body: JSON.stringify(toy)
   })
   .then(resp => resp.json())
 }
 
-// return object of an individual toy
-const getToy = id => {
-  return fetch(`http://localhost:3000/toys/${id}`)
-    .then(resp => resp.json())
-}
-
-// Return thr updated toy object
-const updateToyLikes = (id, name, image, likes) => {
-  return fetch(`http://localhost:3000/toys/${id}`, {
+// update toy
+function updateToy(toy) {
+  return fetch(`http://localhost:3000/toys/${toy.id}`, {
     method: 'PATCH',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name: name, image: image, likes: likes += 1})
-  }).then(resp => resp.json())
+    body: JSON.stringify(toy)
+  })
+  .then(resp => resp.json())
 }
+
+//--------------------- On page load ----------------------------
+
+function initialization() {
+  getToys()
+    .then(renderToys);
+  addEventListenerToForm();
+}
+
+initialization()
